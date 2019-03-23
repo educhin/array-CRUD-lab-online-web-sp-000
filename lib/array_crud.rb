a4adc8da43208a53b97c02d24ce2903e9f355fb3
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-23 15:57:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,12 +24,12 @@ def remove_element_from_start_of_array(array)
   array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-
+def retrieve_element_from_index(array, index)
+  array[index]
 end
 
 def retrieve_first_element_from_array(array)
-
+  
 end
 
 def retrieve_last_element_from_array(array)
